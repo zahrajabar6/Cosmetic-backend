e3d3d489efbd020be0d7e65302dc02661b1cfb3c
Fatima Salah <alanyfsalah@gmail.com>
2022-09-16 01:35:09 +0300

    Auth models

---
+++ Account/schemas.py
@@ -0,0 +1,42 @@
+from ninja import Schema
+from typing import Optional
+from pydantic import EmailStr, Field
+
+
+class AccountCreate(Schema):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password1: str = Field(min_length=8)
+    password2: str
+
+class AccountOut(Schema):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: Optional[str]
+    address1: str = None
+
+class SigninSchema(Schema):
+    email: EmailStr
+    password: str
+
+class AccountUpdate(Schema):
+    first_name: str
+    last_name: str
+    phone_number: Optional[str]
+    address1: str
+
+class ChangePasswordSchema(Schema):
+    old_password: str
+    new_password1: str
+    new_password2: str
+
+class TokenOut(Schema):
+    access: str
+
+
+class AuthOut(Schema):
+    token: TokenOut
+    account: AccountOut
+
---
+++ Account/views.py
@@ -0,0 +1,61 @@
+from Account.schemas import *
+from Cosmetic.schemas import MessageOut
+from ninja import Router
+from django.contrib.auth import get_user_model, authenticate
+from Account.authorization import *
+from Account.utils.decorators import *
+User = get_user_model()
+
+account_controller = Router(tags=['auth'])
+
+
+@account_controller.post('signup', response={
+    400: MessageOut,
+    201: AuthOut,
+})
+def signup(request, account_in: AccountCreate):
+    if account_in.password1 != account_in.password2:
+        return 400, {'detail': 'Passwords do not match!'}
+
+    try:
+        User.objects.get(email=account_in.email)
+    except User.DoesNotExist:
+        new_user = User.objects.create_user(
+            first_name=account_in.first_name,
+            last_name=account_in.last_name,
+            email=account_in.email,
+            password=account_in.password1
+        )
+
+        token = get_tokens_for_user(new_user)
+
+        return 201, {
+            'token': token,
+            'account': new_user,
+        }
+
+    return 400, {'detail': 'User already registered!'}
+
+@account_controller.post('signin', response={
+    200: AuthOut,
+    404: MessageOut,
+})
+def signin(request, signin_in: SigninSchema):
+    user = authenticate(email=signin_in.email, password=signin_in.password)
+
+    if not user:
+        return 404, {'detail': 'User does not exist'}
+
+    token = get_tokens_for_user(user)
+
+    return {
+        'token': token,
+        'account': user
+    }
+
+@account_controller.get('', auth=GlobalAuth(), response={
+    200: AccountOut,
+    401: MessageOut})
+@check_pk
+def me(request):
+    return User.objects.get(id=request.auth['pk'])
